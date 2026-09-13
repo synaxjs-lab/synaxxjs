2fceaf1ec1ef655d201059cc4f572ce1094285c9
synaxjs-lab <synax.js@gmail.com>
2026-09-13 16:27:33 +0530

    Update ChatRoom.tsx

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -255,7 +255,7 @@ export const ChatRoom: React.FC<ChatRoomProps> = ({
         <div className="flex items-center gap-3">
           <div className="relative">
             <img
-              src={otherUser.pfpUrl}}
+              src={otherUser.pfpUrl}
               alt={otherUser.name}
               className="w-10 h-10 rounded-full object-cover border-2 border-indigo-500/50 shadow-[0_0_15px_rgba(99,102,241,0.2)]"
             />
